93c6d047284387d1f7bedf1f094602d245ac29b2
r1b4z01d <bryanribas@gmail.com>
2019-06-09 20:08:40 -0500

    firmware

---
+++ PlayaBikeFirmware/PlayaBikeFirmware.ino
@@ -0,0 +1,378 @@
+#include <FastLED.h>
+
+FASTLED_USING_NAMESPACE
+
+uint8_t ledMode = 0;
+uint8_t maxMode = 6;
+
+int eepaddress = 0;                                           // Store the starting mode in EEPROM address 0.
+
+// Use qsuba for smooth pixel colouring and qsubd for non-smooth pixel colouring
+#define qsubd(x, b)  ((x>b)?b:0)                              // Digital unsigned subtraction macro. if result <0, then => 0. Otherwise, take on fixed value.
+#define qsuba(x, b)  ((x>b)?x-b:0)                            // Analog Unsigned subtraction macro. if result <0, then => 0
+
+#define DATA_PIN    5
+#define potPin   12
+#define LED_TYPE    WS2812
+#define COLOR_ORDER GRB
+#define NUM_LEDS    93
+CRGB leds[NUM_LEDS];
+CRGB clr1;
+CRGB clr2;
+uint8_t speed;
+uint8_t loc1;
+uint8_t loc2;
+uint8_t ran1;
+uint8_t ran2;
+
+uint8_t frequency = 50;                                       // controls the interval between strikes
+uint8_t flashes = 8;                                          //the upper limit of flashes per strike
+unsigned int dimmer = 1;
+
+uint8_t ledstart;                                             // Starting location of a flash
+uint8_t ledlen;                                               // Length of a flash
+
+
+#define BRIGHTNESS       100
+#define FRAMES_PER_SECOND  120
+
+// Palette definitions
+CRGBPalette16 currentPalette = PartyColors_p;
+CRGBPalette16 targetPalette;
+TBlendType    currentBlending = LINEARBLEND; 
+
+// Define variables used by the sequences.
+int   thisdelay =   10;                                       // A delay value for the sequence(s)
+uint8_t   count =   0;                                        // Count up to 255 and then reverts to 0
+uint8_t fadeval = 224;                                        // Trail behind the LED's. Lower => faster fade.
+
+// Initialize global variables for sequences.
+uint8_t    palIndex =  95;
+uint8_t    thissat = 255;
+int        thisdir =   0;
+uint8_t thisbright = 255;
+bool        huerot =   0;                                     // Does the hue rotate? 1 = yes
+uint8_t      bgclr =   0;
+uint8_t      bgbri =   0;
+uint8_t bpm = 30;
+
+void setup() {
+  delay(3000); // 3 second delay for recovery
+    Serial.begin(115200);                                        // Initialize serial port for debugging.
+  delay(1000);                                                // Soft startup to ease the flow of electrons.
+
+  // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE,5,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE,6,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE,7,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE,8,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  // set master brightness control
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+
+// List of patterns to cycle through.  Each is defined as a separate function below.
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = {  ChangeMe, lightning, blur,fill_grad, blendwave, beatwave,fadein,dot_beat,rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm2 };
+uint8_t gCurrentPatternNumber = 0; // 14 Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+
+void setBrightness(){
+  int val = analogRead(potPin);
+  val = map(val, 0, 1023, 10, 200);
+  analogWrite(9, val);
+  FastLED.setBrightness(val);
+  Serial.println(val);
+
+}
+
+void loop()
+{
+  // Call the current patt\ern function once, updating the 'leds' array
+  gPatterns[gCurrentPatternNumber]();
+
+  // send the 'leds' array out to the actual LED strip
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+ EVERY_N_MILLISECONDS(100) {                                                 // FastLED based non-blocking FIXED delay.
+    uint8_t maxChanges = 24;
+    nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges);    // AWESOME palette blending capability.
+    setBrightness();
+  }
+
+  EVERY_N_SECONDS(5) {                                                        // Change the target palette to a random one every 5 seconds.
+    uint8_t baseC = random8(255);                                             // Use the built-in random number generator as we are re-initializing the FastLED one.
+    targetPalette = CRGBPalette16(CHSV(baseC+random8(0,32), 255, random8(128, 255)), CHSV(baseC+random8(0,32), 255, random8(128, 255)), CHSV(baseC+random8(0,32), 192, random8(128, 255)), CHSV(baseC+random8(0,32), 255, random8(128, 255)));
+  }
+
+if(gCurrentPatternNumber==0){
+   ChangeMe();
+
+  EVERY_N_MILLISECONDS(thisdelay) {
+    matrix();
+  }
+  
+  FastLED.show();
+  
+  }
+
+  // do some periodic updates
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+}
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+void nextPattern()
+{
+  // add one to the current pattern number, and wrap around at the end
+  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
+
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void rainbowWithGlitter() 
+{
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+void matrix() {                                               // One line matrix
+
+  if (huerot) palIndex++;
+  
+  if (random8(90) > 80) {
+    if (thisdir == 0)
+      leds[0] = ColorFromPalette(currentPalette, palIndex, thisbright, currentBlending); 
+    else
+      leds[NUM_LEDS-1] = ColorFromPalette( currentPalette, palIndex, thisbright, currentBlending);
+  } else {
+    if (thisdir == 0)
+      leds[0] = CHSV(bgclr, thissat, bgbri); 
+    else
+      leds[NUM_LEDS-1] = CHSV(bgclr, thissat, bgbri);
+  }
+
+  if (thisdir == 0) {
+    for (int i = NUM_LEDS-1; i >0 ; i-- ) leds[i] = leds[i-1];
+  } else {
+    for (int i = 0; i < NUM_LEDS-1 ; i++ ) leds[i] = leds[i+1];
+  }
+  
+} // matrix()
+
+
+
+// Also RainbowColors_p, RainbowStripeColors_p, OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
+
+void ChangeMe() {                                             // A time (rather than loop) based demo sequencer. This gives us full control over the length of each sequence.
+  
+  uint8_t secondHand = (millis() / 1000) % 25;                // Change '25' to a different value to change length of the loop.
+  static uint8_t lastSecond = 99;                             // Static variable, means it's only defined once. This is our 'debounce' variable.
+
+  if (lastSecond != secondHand) {                             // Debounce to make sure we're not repeating an assignment.
+    lastSecond = secondHand;
+    switch(secondHand) {
+      case  0: thisdelay=50; palIndex=95; bgclr=140; bgbri=4; huerot=0; break;
+      case  5: targetPalette = OceanColors_p; thisdir=1; bgbri=0; huerot=1; break;
+      case 10: targetPalette = LavaColors_p; thisdelay=30; palIndex=0; bgclr=50; bgbri=8; huerot=0; break;
+      case 15: targetPalette = ForestColors_p; thisdelay=80; bgbri = 16; bgclr=96; palIndex=random8(); break;
+      case 20: palIndex=random8(); huerot=1; break;
+      case 25: break;
+    }
+  }
+
+} // ChangeMe()
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm2()
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
+
+void fadein() {
+
+  random16_set_seed(535);                                                           // The randomizer needs to be re-set each time through the loop in order for the 'random' numbers to be the same each time through.
+
+  for (int i = 0; i<NUM_LEDS; i++) {
+    uint8_t fader = sin8(millis()/random8(10,20));                                  // The random number for each 'i' will be the same every time.
+    leds[i] = ColorFromPalette(currentPalette, i*20, fader, currentBlending);       // Now, let's run it through the palette lookup.
+  }
+
+  random16_set_seed(millis());                                                      // Re-randomizing the random number seed for other routines.
+
+}// fadein()
+
+
+
+
+void ease() {
+
+  static uint8_t easeOutVal = 0;
+  static uint8_t easeInVal  = 0;
+  static uint8_t lerpVal    = 0;
+
+  easeOutVal = ease8InOutQuad(easeInVal);                     // Start with easeInVal at 0 and then go to 255 for the full easing.
+  easeInVal++;
+
+  lerpVal = lerp8by8(0, NUM_LEDS, easeOutVal);                // Map it to the number of LED's you have.
+
+  leds[lerpVal] = CRGB::Red;
+  fadeToBlackBy(leds, NUM_LEDS, 16);                          // 8 bit, 1 = slow fade, 255 = fast fade
+  
+} // ease()
+
+
+void beatwave() {
+  
+  uint8_t wave1 = beatsin8(9, 0, 255);                        // That's the same as beatsin8(9);
+  uint8_t wave2 = beatsin8(8, 0, 255);
+  uint8_t wave3 = beatsin8(7, 0, 255);
+  uint8_t wave4 = beatsin8(6, 0, 255);
+
+  for (int i=0; i<NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette( currentPalette, i+wave1+wave2+wave3+wave4, 255, currentBlending); 
+  }
+  
+} // beatwave()
+void dot_beat() {
+
+  uint8_t inner = beatsin8(bpm, NUM_LEDS/4, NUM_LEDS/4*3);    // Move 1/4 to 3/4
+  uint8_t outer = beatsin8(bpm, 0, NUM_LEDS-1);               // Move entire length
+  uint8_t middle = beatsin8(bpm, NUM_LEDS/3, NUM_LEDS/3*2);   // Move 1/3 to 2/3
+
+  leds[middle] = CRGB::Purple;
+  leds[inner] = CRGB::Blue;
+  leds[outer] = CRGB::Aqua;
+
+  nscale8(leds,NUM_LEDS,fadeval);                             // Fade the entire array. Or for just a few LED's, use  nscale8(&leds[2], 5, fadeval);
+
+} // dot_beat()
+
+
+void blendwave() {
+
+  speed = beatsin8(6,0,255);
+
+  clr1 = blend(CHSV(beatsin8(3,0,255),255,255), CHSV(beatsin8(4,0,255),255,255), speed);
+  clr2 = blend(CHSV(beatsin8(4,0,255),255,255), CHSV(beatsin8(3,0,255),255,255), speed);
+
+  loc1 = beatsin8(10,0,NUM_LEDS-1);
+  
+  fill_gradient_RGB(leds, 0, clr2, loc1, clr1);
+  fill_gradient_RGB(leds, loc1, clr2, NUM_LEDS-1, clr1);
+
+} // blendwave()
+void black(){
+
+  fill_solid(leds, NUM_LEDS, CRGB::Black);                    // Just to be sure, let's really make it BLACK.
+  FastLED.show();                         // Power managed display
+  
+  
+}
+
+
+
+void lightning() {
+  
+  ledstart = random8(NUM_LEDS);                               // Determine starting location of flash
+  ledlen = random8(NUM_LEDS-ledstart);                        // Determine length of flash (not to go beyond NUM_LEDS-1)
+  
+  for (int flashCounter = 0; flashCounter < random8(3,flashes); flashCounter++) {
+    if(flashCounter == 0) dimmer = 5;                         // the brightness of the leader is scaled down by a factor of 5
+    else dimmer = random8(1,3);                               // return strokes are brighter than the leader
+    
+    fill_solid(leds+ledstart,ledlen,CHSV(255, 0, 255/dimmer));
+    FastLED.show();                       // Show a section of LED's
+    delay(random8(4,10));                                     // each flash only lasts 4-10 milliseconds
+    fill_solid(leds+ledstart,ledlen,CHSV(255,0,0));           // Clear the section of LED's
+    FastLED.show();
+    
+    if (flashCounter == 0) delay (150);                       // longer delay until next flash after the leader
+    
+    delay(50+random8(100));                                   // shorter delay between strokes  
+  } // for()
+  
+  delay(random8(frequency)*100);                              // delay between strikes
+  
+} // loop()
+
+
+void blur() {
+
+  uint8_t blurAmount = dim8_raw( beatsin8(3,64, 192) );       // A sinewave at 3 Hz with values ranging from 64 to 192.
+  blur1d( leds, NUM_LEDS, blurAmount);                        // Apply some blurring to whatever's already on the strip, which will eventually go black.
+  
+  uint8_t  i = beatsin8(  9, 0, NUM_LEDS);
+  uint8_t  j = beatsin8( 7, 0, NUM_LEDS);
+  uint8_t  k = beatsin8(  5, 0, NUM_LEDS);
+  
+  // The color of each point shifts over time, each at a different speed.
+  uint16_t ms = millis();  
+  leds[(i+j)/2] = CHSV( ms / 29, 200, 255);
+  leds[(j+k)/2] = CHSV( ms / 41, 200, 255);
+  leds[(k+i)/2] = CHSV( ms / 73, 200, 255);
+  leds[(k+i+j)/3] = CHSV( ms / 53, 200, 255);
+  
+  FastLED.show();
+  
+} // blur()
+ 
+
+
+void fill_grad() {
+  
+  uint8_t starthue = beatsin8(5, 0, 255);
+  uint8_t endhue = beatsin8(7, 0, 255);
+  
+  if (starthue < endhue) {
+    fill_gradient(leds, NUM_LEDS, CHSV(starthue,255,255), CHSV(endhue,255,255), FORWARD_HUES);    // If we don't have this, the colour fill will flip around. 
+  } else {
+    fill_gradient(leds, NUM_LEDS, CHSV(starthue,255,255), CHSV(endhue,255,255), BACKWARD_HUES);
+  }
+  
+} // fill_grad()
